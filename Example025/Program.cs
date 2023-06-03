f38819d30735ef0607ff7ccb420ebd58281df2dc
Olga <olechka.mir28@mail.ru>
2023-06-03 08:21:14 +0300

    Example025

---
+++ Example025/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+//2, 4 -> 16
+Console.Clear();
+Console.WriteLine("Введите число a: ");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число b: ");
+int b = int.Parse(Console.ReadLine());
+
+int GetExponentiation(int b, int a)
+{
+    int exp = a;
+    for (int i = 1; i < b; i++)
+    {
+        exp = exp * a;  
+    }
+    return exp;
+}
+Console.WriteLine($"Число {a} в степени {b} равно {GetExponentiation(b, a)}");
